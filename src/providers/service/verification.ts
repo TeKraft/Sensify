f4448d2b4a319e91d34135ca22ec21ab3130e53a
Viktor Gorte <v_gort01@uni-muenster.de>
2019-01-25 11:53:07 +0100

    Cleanup and commenting of verification.ts

--- src/providers/service/verification.ts
+++ src/providers/service/verification.ts
@@ -7,33 +7,82 @@ export class verification {
     constructor() {
     }
 
-    //Returns the mean sensor value of all closest Boxes (between -Current Day ONLY!!)
-    getMeanValue(sensorName: String, senseBoxes: SenseBox[]) {
-        let sum: number = 0;
-        let numberOfSensors = 0;
-        let mean: number;
-        //needed to check for same day measurements
+    /**
+     *  Function for getting the current date, seperated into day, month and year.
+     *  @return Object with current day, month and year.
+     */
+    setCurrentDate(){
         let date = new Date();
         let currentDay = date.getUTCDate();
         let currentMonth = date.getUTCMonth() + 1;  //January is 0
         let currentYear = date.getUTCFullYear();
+        return {
+            currentDay:currentDay,
+            currentMonth:currentMonth,
+            currentYear:currentYear
+        }
+    };
 
+    /**
+     * Function for dividing the createdAt string from the SenseBoxes to seperate the day, month and year.
+     * @param measurements Object with the date of the measurements and the valmeasured value.
+     * @return Object with date of the given measurement, given in day, month and year.
+     */
+    getMeasurementDate(measurements:any){
+        let creationDay = Number(measurements.createdAt.substring(8, 10));
+        let creationMonth = Number(measurements.createdAt.substring(5, 7));
+        let creationYear = Number(measurements.createdAt.substring(0, 4));
+        return {
+            day:creationDay,
+            month:creationMonth,
+            year:creationYear
+        }
+    }
+
+    /**
+     * Function for verifying if a given value is equal.
+     * @param mean Mean value of all surrounding SenseBoxes with the same sensor as the boxValue.
+     * @param range Verification range.
+     * @param boxValue SenseBox sensor value.
+     * @return Boolean, indicating if the given value is in the verification range.
+     */
+    valueInVerificationRange(mean:number, range:number, boxValue:number){
+        const upperLimit = mean + range;
+        const lowerLimit = mean - range;
+
+        if (lowerLimit <= boxValue && boxValue <= upperLimit) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Function for calculating the mean sensor value for a given sensor name and a set of SenseBoxes.
+     * First, the existence of the sensor in the given SenseBox is verified.
+     * After verifying that the measurements from those sensors are up to date, the mean of all sensor values 
+     * is calculated and returned. 
+     * @param sensorName Name of the sensor for which the mean value has to be calculated.
+     * @param senseBoxes Array of SenseBoxes.
+     * @return Mean value of all sensors that match the given sensor type and that have values that are up to date. If there are no senseboxes, 0 is returned.
+     */
+    getMeanValue(sensorName:String, senseBoxes:SenseBox[]) {
+        let sum:number = 0;
+        let numberOfSensors = 0;
+        let mean:number;
+        //needed to check for same day measurements
+        const today = this.setCurrentDate();
         //check all closest boxes
         senseBoxes.forEach(box => {
-            //check all sensors
+            //No Boxes found check
             if (box) {
                 box.sensors.forEach(sensor => {
                     //if sensor title is equal to searched sensor
                     if (sensor.title == sensorName && sensor.lastMeasurement && sensor.lastMeasurement.createdAt) {
                         let measurements = sensor.lastMeasurement;
-
-                        //Get Year, Month, Day of lastMeasurements for checking
-                        let creationDay = Number(measurements.createdAt.substring(8, 10));
-                        let creationMonth = Number(measurements.createdAt.substring(5, 7));
-                        let creationYear = Number(measurements.createdAt.substring(0, 4));
-
+                        let measurementDate = this.getMeasurementDate(measurements)
                         //check if last measurement is from same day, same month, same year
-                        if (creationDay == currentDay && creationMonth == currentMonth && creationYear == currentYear) {
+                        if (measurementDate.day == today.currentDay && measurementDate.month == today.currentMonth && measurementDate.year == today.currentYear) {
                             sum += Number(measurements.value);
                             numberOfSensors++;
                         }
@@ -43,7 +92,8 @@ export class verification {
         });
         //Division by zero handling
         if (numberOfSensors === 0) {
-            console.error("VALIDATION ERROR: Could not verify. No Sensors or no measurements from today found.");
+            console.error("VALIDATION ERROR:Could not verify. No Sensors or no measurements from today found.");
+            alert("Validation Error!")
             return 0;
         } else {
             mean = sum / numberOfSensors;
@@ -51,12 +101,17 @@ export class verification {
         }
     }
 
-    //Checks if closestSenseBox has the sensor that you want to verify
-    senseBoxHasSensor(sensorName: String, closestBox: SenseBox) {
+    /**
+     * Function for verifying the existence of a given sensor in a given SenseBox.
+     * @param sensorName Name of the sensor
+     * @param closestBox SenseBox for which the existence has to be verified.
+     * @return Boolean, which indicates the existence of absence of the given sensor in the given SenseBox.
+     */
+    senseBoxHasSensor(sensorName:String, closestBox:SenseBox) {
         let res = false;
         if (closestBox) {
             closestBox.sensors.forEach(sensor => {
-                let title: String = sensor.title;
+                let title:String = sensor.title;
                 if (sensorName === title) res = true;
             });
         }
@@ -65,14 +120,14 @@ export class verification {
 
     //
     /**
-     * Function for validating a sensor measurement by comparing it to the mean value of the closest SenseBox measurements (+/- some range).
-     * @param sensorName    {String}    Name of the Sensor, for instance: "Temperatur".
-     * @param closestBox    {SenseBox}    SenseBox the user is connected to.
-     * @param senseBoxes    {SenseBox[]}Array of all SenseBoxes inside certain radius    .
-     * @param range        {number}    Verification range.
-     * @return {boolean}                True, if value is inside range.
+     * Function for verifying a sensor measurement by comparing it to the mean value of the closest SenseBox measurements (+/- a set range).
+     * @param sensorName  Name of the Sensor, for instance:"Temperatur".
+     * @param closestBox  SenseBox the user is connected to.
+     * @param senseBoxes  Array of all SenseBoxes inside certain radius    .
+     * @param range       Verification range.
+     * @return True, if value is inside range.
      */
-    sensorIsValid(sensorName: String, closestBox: SenseBox, senseBoxes: SenseBox[], range: number) {
+    sensorIsValid(sensorName:String, closestBox:SenseBox, senseBoxes:SenseBox[], range:number) {
         let valiValue = false;
         //Check if closestBox even has the given sensor
         if (this.senseBoxHasSensor(sensorName, closestBox)) {
@@ -84,9 +139,17 @@ export class verification {
         return (valiValue);
     };
 
-    //Checks if sensor value form sensebox is inside the range of mean values of all closest boxes (+/- verification range)
-    verifyValue(sensorName: String, mean: number, closestBox: SenseBox, range: number) {
-        let boxValue: number;
+    /**
+     * Function for verifying the latest measurement of a given sensor in a given SenseBox.
+     * The value is verified if the mean value of all surrounding boxes, +/- a given range, includes the value to be verified.
+     * @param sensorName Name of the sensor.
+     * @param mean Mean value, of the same sensors, of all the surrounding SenseBoxes.
+     * @param closestBox The current SenseBox for which the last sensor measurement has to be verified.
+     * @param range Pre-set or user-set range round the mean for which a value is verified or not.
+     * @return Boolean, indicating if the value of the given sensor if verified.
+     */
+    verifyValue(sensorName:String, mean:number, closestBox:SenseBox, range:number) {
+        let boxValue:number;
         //get closest box value for verification
         closestBox.sensors.forEach(sensor => {
             if (sensor.title === sensorName) {
@@ -96,12 +159,7 @@ export class verification {
                 boxValue = Number(sensor.lastMeasurement.value);
             }
         });
-        //if boxvalue is inside range, return true, else false
-        if (mean - range <= boxValue && boxValue <= mean + range) {
-            return true;
-        } else {
-            return false;
-        }
+       //if boxvalue is inside range, return true, else false
+       return this.valueInVerificationRange(mean, range, boxValue);
     }
-
 }
